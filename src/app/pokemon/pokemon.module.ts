93c01b70e3ee91ab02e5f8b7038b77e54309d031
boye.claude <boye.claude@gmail.com>
2022-05-20 18:09:36 +0100

    template form: finish

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -5,15 +5,26 @@ import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.componen
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { BorderCardDirective } from "./border-card.directive";
 import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
+import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
 
 const pokemonRoutes: Routes = [
+  { path: "edit/pokemon/:id", component: EditPokemonComponent },
   { path: "pokemons", component: ListPokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent },
 ];
 
 @NgModule({
-  declarations: [ListPokemonComponent, DetailPokemonComponent, PokemonTypeColorPipe, BorderCardDirective],
+  declarations: [
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    PokemonTypeColorPipe,
+    BorderCardDirective,
+    PokemonFormComponent,
+    EditPokemonComponent,
+  ],
   exports: [ListPokemonComponent, DetailPokemonComponent],
-  imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoutes)],
 })
 export class PokemonModule {}
